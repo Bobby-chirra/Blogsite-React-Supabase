ba62e44aa4d6e4ec50cee569565c5f4ab62a2f3f
Bobby-chirra <chbobby184@gmail.com>
2025-11-21 17:02:31 +0530

    keys

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://thxohmhvyvrppuyscias.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRoeG9obWh2eXZycHB1eXNjaWFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3MTQyNzAsImV4cCI6MjA3OTI5MDI3MH0.sgEpL6HGoEo4iv0ONjTWi6tmdvjT_0OVxUhjc5_XHAo';
-
+const supabaseUrl = 'your_supabase_url';
+const supabaseKey = 'your_anon_key'
 export const supabase = createClient(supabaseUrl, supabaseKey);
-
-
