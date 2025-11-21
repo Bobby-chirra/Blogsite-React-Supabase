fbb4e7f813a622e3cccecf1c7cd08a78c5ed0333
Bobby-chirra <chbobby184@gmail.com>
2025-11-21 17:12:48 +0530

    keys enabling

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,11 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'your_supabase_url';
-const supabaseKey = 'your_anon_key'
+// use these for local purposes
+// const supabaseUrl = 'your_supabase_url';
+// const supabaseKey = 'your_anon_key'
 
 // use these when deploying your site:
-/*
+
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-*/
-export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
